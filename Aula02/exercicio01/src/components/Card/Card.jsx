41605961500bc3eb6e126651e5a16f901ed9afc3
Joel Santos <i3975@rn.senac.local>
2025-08-04 17:09:20 -0300

    feat: Aula 02- Projeto React e organização de componentes/pages

---
+++ Aula02/exercicio01/src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Card.module.css";
+
+function Card(){
+    return(
+        <>
+            <div className={styles.container}> 
+                <h1>Card</h1>
+            </div>
+        </>
+    )
+}
+
+export default Card;
---
+++ Aula02/exercicio01/src/components/Main/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Main.module.css";
+import Card from "../Card/Card";
+
+function Main() {
+    return (
+        <>
+            <main className={styles.main}>
+                <Card />
+                <Card />
+
+            </main>
+        </>
+    )
+}
+
+export default Main;
